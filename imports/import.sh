c9eba00082d60aecf9161a5bd8375eff68f7b308
Denis Talaluev <denis.tal@mail.ru>
2025-07-05 08:39:23 +1000

    feat(main): Изменить вывод в скриптах

--- imports/import.sh
+++ imports/import.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Запуск script1.py..."
+echo "Запуск import_logdays.py..."
 python3 import_logdays.py
 
-echo "Запуск script2.py..."
+echo "Запуск import_needs.py..."
 python3 import_needs.py
 
-echo "Запуск script3.py..."
+echo "Запуск import_stores.py..."
 python3 import_stores.py
 
 exit 0
